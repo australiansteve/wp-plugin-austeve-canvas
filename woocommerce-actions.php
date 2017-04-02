c0a8d752da3ffaccbe6d11d67b85e67f5d223705
Steve weaver Crawford <steve@australiansteve.com>
2017-04-02 15:28:28 -0300

    Stop expired events from being added to the cart

--- woocommerce-actions.php
+++ woocommerce-actions.php
@@ -28,4 +28,23 @@ function austeve_woocommerce_return_to_home( ) {
 <?php
 }
 
+
+/**
+ * Process items as they are added to the cart
+ */
+function austeve_filter_woocommerce_add_cart_item_data( $cart_item_data, $product_id, $variation_id ) { 
+
+	$now = new DateTime();
+	$wc_expiry = get_field('_expiration_date', $product_id);
+
+	//If an event has already expired, stop it from being added to the cart
+	if ($wc_expiry && $wc_expiry < $now)
+	{
+        throw new Exception( __( 'Sorry, this event has already expired and tickets can no longer be purchased.', 'woocommerce' ) );
+	}
+
+    return $cart_item_data; 
+}; 
+add_filter( 'woocommerce_add_cart_item_data', 'austeve_filter_woocommerce_add_cart_item_data', 10, 3 ); 
+
 ?>
