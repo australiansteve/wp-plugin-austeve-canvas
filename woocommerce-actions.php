4b934afa72177c6836802bc2abc515bfcdc28dd4
Steve weaver Crawford <steve@australiansteve.com>
2017-04-28 08:58:31 -0300

    WooCommerce action moved into theme

--- woocommerce-actions.php
+++ woocommerce-actions.php
@@ -13,22 +13,6 @@ function austeve_woocommerce_auto_complete_order( $order_id ) {
     $order->update_status( 'completed' );
 }
 
-
-/**
- * Add 'Return to Home' button after displaying order details.
- */
-add_action( 'woocommerce_thankyou', 'austeve_woocommerce_return_to_home', 50, 0 );
-function austeve_woocommerce_return_to_home( ) { 
-	error_log("Return home");
-
-?>
-    <div class='row columns text-center'>
-    <a class='button' href='<?php echo site_url();?>'>Return home</a>
-    </div>
-<?php
-}
-
-
 /**
  * Process items as they are added to the cart
  */
